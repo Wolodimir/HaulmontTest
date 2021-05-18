dd30919c17ef4b79eb64772b8fde43486a0122cf
Wolodimir <dvdent666@gmail.com>
2021-05-18 17:25:47 +0400

    Maven init

---
+++ src/test/java/com/task/haulmonttest/HaulmontTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.haulmonttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulmontTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
